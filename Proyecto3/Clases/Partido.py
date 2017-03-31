aae82063d092a8c165e839c2d80d5943c9fdbf8a
EmpanadasDeLaBufe <juanchi.xeneise@live.com.ar>
2017-03-31 09:26:15 -0300

    first commit

---
+++ Proyecto3/Clases/Partido.py
@@ -0,0 +1,11 @@
+class Partido(object):
+
+    Dia=None
+
+    Turno=None
+
+    Equipo1=None
+
+    Equipo2=None
+
+    Ganador=None
---
+++ Proyecto3/Clases/Torneo.py
@@ -0,0 +1,13 @@
+from .Equipos import Equipos
+from .Partido import Partido
+class Torneo(object):
+    Equipos=[]
+
+    Partidos=[]
+
+    def agregarEquipos(self, n):
+        for variable in self.Equipos:
+            if variable.Nombre==n.Nombre:
+                return False
+        self.Equipos.append(n)
+        return True
